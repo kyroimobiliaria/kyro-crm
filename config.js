aeb7198313fb26628220eecf7f6f74ced7f48563
kyroimobiliaria <kyroimobiliaria@gmail.com>
2026-07-07 19:18:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
       DEMO_MODE = false -> exige login por e-mail/senha (uso real em equipe)
    ========================================================================= */
 const CONFIG = {
-  SUPABASE_URL: 'SUA_URL_AQUI',
-  SUPABASE_ANON_KEY: 'SUA_CHAVE_ANON_AQUI',
+  SUPABASE_URL: 'https://jnoaxevyevxfkeykvblk.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impub2F4ZXZ5ZXZ4ZmtleWt2YmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NTI2ODgsImV4cCI6MjA5OTAyODY4OH0.KVrKnF7nd2mtU6McchRVZb21_OTvIyhsXjrqCvyXdhQ',
   DEMO_MODE: true,
 };
